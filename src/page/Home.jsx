82982704cba91736d83523f415828209f93b50be
Ledeuadian <adrianjrfabian@gmail.com>
2023-05-26 12:09:21 +0800

    fixed watch btn placement

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -30,10 +30,7 @@ export default function Home() {
       <div className="d-flex flex-row container-fluid justify-content-evenly bg-dark">
         <div className="container-fluid d-flex flex-column align-items-center text-white text-center p-5">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
-            exercitationem consectetur magni accusantium, quod recusandae iste
-            rem? Asperiores, ipsum itaque sequi optio ipsa tempore debitis
-            officia dignissimos? Est, ea atque?
+          Andy Dufresne (Tim Robbins) is sentenced to two consecutive life terms in prison for the murders of his wife and her lover and is sentenced to a tough prison. However, only Andy knows he didn't commit the crimes. While there, he forms a friendship with Red (Morgan Freeman), experiences brutality of prison life, adapts, helps the warden, etc., all in 19 years.
           </p>
           <div className="d-flex mt-auto mb-5 justify-content-evenly">
             <Button className="me-2" variant="danger">
@@ -68,11 +65,11 @@ export default function Home() {
           {movies.map((movie) => (
             <div className="item row-wrapper">
               <Row>
-                <Card style={{ width: "18rem" }}>
+                <Card style={{ width: "15rem" }}>
                   <Card.Img variant="top" src={movie.img} />
                   <Card.Body>
                     <Card.Title>{movie.name}</Card.Title>
-                    <Button variant="primary">Watch</Button>
+                    <Button variant="primary" className="buttonMarge">Watch</Button>
                   </Card.Body>
                 </Card>
               </Row>
@@ -99,7 +96,7 @@ export default function Home() {
           {series.map((series) => (
             <div className="item row-wrapper">
               <Row>
-                <Card style={{ width: "18rem" }}>
+                <Card style={{ width: "15rem" }}>
                   <Card.Img variant="top" src={series.img} />
                   <Card.Body>
                     <Card.Title>{series.name}</Card.Title>
